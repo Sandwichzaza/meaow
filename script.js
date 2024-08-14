77dcbb9491f170cf117fe07dcffec3ae1c11b8b0
Sandwichzaza <sandwith1g@gmail.com>
2024-08-14 23:54:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Ryu is chubby ! 😘";
+  question.innerHTML = "kwan so cute !!! 😘";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
