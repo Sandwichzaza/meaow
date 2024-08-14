ee075bb8cf4277e48815d66d598fa5483866eb18
Sandwichzaza <sandwith1g@gmail.com>
2024-08-15 00:08:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "kwan so cute !!! 😘";
+  question.innerHTML = "NO !!! Kwan so Cute !!! 😘";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
